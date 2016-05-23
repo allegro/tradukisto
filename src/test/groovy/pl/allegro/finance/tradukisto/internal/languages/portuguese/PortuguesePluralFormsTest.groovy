9fba49059b65432f91b153f11196557f9ba42bff
Ralph Avalon <amoedo.raphael@gmail.com>
2016-05-23 16:52:53 -0300

    Adding more tests for portuguese converter

---
+++ src/test/groovy/pl/allegro/finance/tradukisto/internal/languages/portuguese/PortuguesePluralFormsTest.groovy
@@ -0,0 +1,22 @@
+package pl.allegro.finance.tradukisto.internal.languages.portuguese
+
+import pl.allegro.finance.tradukisto.internal.languages.GenderType
+import spock.lang.Specification
+
+class PortuguesePluralFormsTest extends Specification {
+
+    def pluralForms = new PortuguesePluralForms("milhão", "milhões", GenderType.NON_APPLICABLE)
+
+    def "should provide value for singular form"() {
+        expect:
+        pluralForms.formFor(1) == "milhão"
+    }
+
+    def "should provide value for plural forms"() {
+        expect:
+        pluralForms.formFor(value) == "milhões"
+
+        where:
+        value << [0] + (2..100)
+    }
+}
