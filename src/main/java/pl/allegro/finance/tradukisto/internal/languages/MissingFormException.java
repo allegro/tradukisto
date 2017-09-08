24eba3b610308aaeac6a6b98768d992651b1ffd7
miroslav.svoboda <miroslav.svoboda.external@telekom.sk>
2017-09-08 19:05:01 +0200

    Review change - remove Serial UID

--- src/main/java/pl/allegro/finance/tradukisto/internal/languages/MissingFormException.java
+++ src/main/java/pl/allegro/finance/tradukisto/internal/languages/MissingFormException.java
@@ -2,8 +2,6 @@ package pl.allegro.finance.tradukisto.internal.languages;
 
 public class MissingFormException extends RuntimeException {
 
-    private static final long serialVersionUID = 1L;
-
     MissingFormException(String message) {
         super(message);
     }
