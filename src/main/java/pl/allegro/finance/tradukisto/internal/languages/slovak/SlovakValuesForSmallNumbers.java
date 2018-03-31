5e3d7552baed06f4923df37f2124168f913c2d50
Dorian Sarnowski <dorian.sarnowski@gmail.com>
2018-03-31 22:57:49 +0200

    Update SlovakValuesForSmallNumbers.java

--- src/main/java/pl/allegro/finance/tradukisto/internal/languages/slovak/SlovakValuesForSmallNumbers.java
+++ src/main/java/pl/allegro/finance/tradukisto/internal/languages/slovak/SlovakValuesForSmallNumbers.java
@@ -6,8 +6,6 @@ import pl.allegro.finance.tradukisto.internal.languages.GenderType;
 import pl.allegro.finance.tradukisto.internal.languages.PluralForms;
 import pl.allegro.finance.tradukisto.internal.languages.czech.CzechPluralForms;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
