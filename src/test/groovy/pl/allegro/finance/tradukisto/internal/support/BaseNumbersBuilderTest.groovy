0591d70f9addc79a216ae8311e2e2490de36ec19
Maciej Lipinski <maciej.lipinski@allegro.pl>
2022-01-31 10:02:21 +0100

    review fixes

--- src/test/groovy/pl/allegro/finance/tradukisto/internal/support/BaseNumbersBuilderTest.groovy
+++ src/test/groovy/pl/allegro/finance/tradukisto/internal/support/BaseNumbersBuilderTest.groovy
@@ -7,7 +7,11 @@ import static pl.allegro.finance.tradukisto.internal.support.BaseNumbersBuilder.
 
 class BaseNumbersBuilderTest extends Specification {
 
-    def builder = baseNumbersBuilder()
+    def builder
+
+    void setup() {
+        builder = baseNumbersBuilder()
+    }
 
     def 'should throw NumberAlreadyMappedException when trying to remap a number with String'() {
         given:
@@ -20,12 +24,34 @@ class BaseNumbersBuilderTest extends Specification {
         thrown(NumberAlreadyMappedException)
     }
 
-    def 'should throw NumberAlreadyMappedException when trying to remap a number with GenderForm'() {
+    def 'should throw NumberAlreadyMappedException when trying to remap a number with GenderForms'() {
         given:
-        builder.put(1, new GenderForms("some form"))
+            builder.put(1, new GenderForms("some form"))
+
+        when:
+            builder.put(1, new GenderForms("some other form"))
+
+        then:
+            thrown(NumberAlreadyMappedException)
+    }
+
+    def 'should throw NumberAlreadyMappedException when trying to remap a number from String to GenderForms'() {
+        given:
+        builder.put(1, "some form")
 
         when:
         builder.put(1, new GenderForms("some other form"))
+
+        then:
+        thrown(NumberAlreadyMappedException)
+    }
+
+    def 'should throw NumberAlreadyMappedException when trying to remap a number from GenderForms to String'() {
+        given:
+        builder.put(1, new GenderForms("some other form"))
+
+        when:
+        builder.put(1, "some form")
 
         then:
         thrown(NumberAlreadyMappedException)
