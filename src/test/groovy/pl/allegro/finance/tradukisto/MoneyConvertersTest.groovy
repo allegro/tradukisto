752ffc668b570bc610c5e733025d7742b3a7b523
rpanecki <birus@wp.pl>
2022-03-15 12:21:20 +0100

    feature/language-independent-currency

--- src/test/groovy/pl/allegro/finance/tradukisto/MoneyConvertersTest.groovy
+++ src/test/groovy/pl/allegro/finance/tradukisto/MoneyConvertersTest.groovy
@@ -109,4 +109,17 @@ class MoneyConvertersTest extends Specification {
         expect:
         TURKISH_BANKING_MONEY_VALUE.asWords(1_23.4) == "YüzYirmiÜçTL,KırkKr."
     }
+
+    def "should convert value with currency symbol provided"() {
+        expect:
+        POLISH_BANKING_MONEY_VALUE.asWords(1_234.56, "EUR") == "jeden tysiąc dwieście trzydzieści cztery EUR 56/100"
+    }
+
+    def "should throw exception when null currency given"() {
+        when:
+        POLISH_BANKING_MONEY_VALUE.asWords(1_234.56, null)
+
+        then:
+        thrown(VerifyException)
+    }
 }
