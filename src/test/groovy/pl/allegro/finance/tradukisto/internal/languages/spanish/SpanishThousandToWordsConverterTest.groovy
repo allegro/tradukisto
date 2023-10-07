d161c4ca274b52ec04fea1e7f6d6ad3916b45e8c
luismateoh <luismateohm@gmail.com>
2023-10-07 10:56:06 -0500

    Add test cases in SpanishThousandToWordsConverterTest

--- src/test/groovy/pl/allegro/finance/tradukisto/internal/languages/spanish/SpanishThousandToWordsConverterTest.groovy
+++ src/test/groovy/pl/allegro/finance/tradukisto/internal/languages/spanish/SpanishThousandToWordsConverterTest.groovy
@@ -1,5 +1,6 @@
 package pl.allegro.finance.tradukisto.internal.languages.spanish
 
+import pl.allegro.finance.tradukisto.internal.MultiFormNumber
 import spock.lang.Specification
 
 import static pl.allegro.finance.tradukisto.internal.languages.GenderForms.genderForm
@@ -48,5 +49,70 @@ class SpanishThousandToWordsConverterTest extends Specification {
         then:
         def exception = thrown(IllegalArgumentException)
         exception.message == "Can't convert 2"
+    }
+
+    def "should return correct words for other thousands with no following number"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([3: genderForm("tres")], [:])
+
+        expect:
+        converter.asWords(3000) == "tres mil"
+    }
+
+    def "should return correct words for other thousands with following hundred"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([3: genderForm("tres")], [100: new MultiFormNumber("cien", "ciento")])
+
+        expect:
+        converter.asWords(3100) == "tres mil cien"
+    }
+
+    def "should return correct words for thousands with other values than hundred"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([5: genderForm("cinco"), 50: genderForm("cincuenta")], [:])
+
+        expect:
+        converter.asWords(5050) == "cinco mil cincuenta"
+    }
+
+    def "should return correct words for thousands with other values than hundred and no following number"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([5: genderForm("cinco"), 50: genderForm("cincuenta")], [:])
+
+        expect:
+        converter.asWords(5000) == "cinco mil"
+    }
+
+    def "should return correct words for 21 with no following number"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([21: genderForm("veintiún"), 20: genderForm("veinte")], [:])
+
+        expect:
+        converter.asWords(21000) == "veintiún mil"
+
+    }
+
+    def "should return correct words for 21 with following hundred"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([21: genderForm("veintiún"), 20: genderForm("veinte")], [100: new MultiFormNumber("cien", "ciento")])
+
+        expect:
+        converter.asWords(21100) == "veintiún mil cien"
+    }
+
+    def "should return correct words for other cases with no following number"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([30: genderForm("treinta"), 1: genderForm("un")], [:])
+
+        expect:
+        converter.asWords(31000) == "treinta y un mil"
+    }
+
+    def "should return correct words for other cases with following hundred"() {
+        given:
+        def converter = new SpanishThousandToWordsConverter([30: genderForm("treinta"), 1: genderForm("un")], [100: new MultiFormNumber("cien", "ciento")])
+
+        expect:
+        converter.asWords(31100) == "treinta y un mil cien"
     }
 }
