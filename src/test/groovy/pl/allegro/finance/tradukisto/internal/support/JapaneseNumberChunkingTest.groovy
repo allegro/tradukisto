8952499f1ac29586f19dcfae71351860bee93a47
tabatad <91326381+tabatad@users.noreply.github.com>
2023-11-05 18:14:51 +0000

    Fix JapaneseNumberChunkingTest

--- src/test/groovy/pl/allegro/finance/tradukisto/internal/support/JapaneseNumberChunkingTest.groovy
+++ src/test/groovy/pl/allegro/finance/tradukisto/internal/support/JapaneseNumberChunkingTest.groovy
@@ -6,7 +6,7 @@ class JapaneseNumberChunkingTest extends Specification {
 
     def splitter = new JapaneseNumberChunking()
 
-    def "should split number to three digit and 2 digit parts as per japanese standards"() {
+    def "should split number to four digit parts as per japanese standards"() {
         expect:
         splitter.chunk(value) == chunks
 
@@ -17,7 +17,7 @@ class JapaneseNumberChunkingTest extends Specification {
         1234           | [1234]
         123000789      | [1, 2300, 789]
         123456789      | [1, 2345, 6789]
-        123000000      | [1, 2300, 0]
+        123000000      | [1, 2300, 0000]
         12300000       | [1230, 0000]
         Long.MAX_VALUE | [922, 3372, 368, 5477, 5807]
     }
