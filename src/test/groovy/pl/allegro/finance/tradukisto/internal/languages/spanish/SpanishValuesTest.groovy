32c13f7a94303bd3221f5979273edc25260e3f4e
Julia Glaszka <85160468+jglaszka@users.noreply.github.com>
2024-04-09 14:18:37 +0200

    Bugfix - spanish money converters (#152)
    * bugfix for spanish numerals + nouns
    
    * clean not needed leftovers
    
    * clean not needed leftovers
    
    * clean unused imports

--- src/test/groovy/pl/allegro/finance/tradukisto/internal/languages/spanish/SpanishValuesTest.groovy
+++ src/test/groovy/pl/allegro/finance/tradukisto/internal/languages/spanish/SpanishValuesTest.groovy
@@ -10,6 +10,7 @@ class SpanishValuesTest extends Specification {
 
     static intConverter = spanishContainer().getIntegerConverter()
     static longConverter = spanishContainer().getLongConverter()
+    static moneyConverter = spanishContainer().getBankingMoneyConverter()
 
     @Unroll
     def "should convert #value to '#words' in Spanish"() {
@@ -144,5 +145,25 @@ class SpanishValuesTest extends Specification {
         1_000_000_000_000_000_000 | ""
         2_000_000_000_000_000_000 | ""
         Long.MAX_VALUE            | ""
+    }
+
+    @Unroll
+    def "should convert money #value to '#words' in Spanish"() {
+        expect:
+        moneyConverter.asWords(value, "€") == words
+
+        where:
+        value   | words
+        51      | "cincuenta y un € 00/100"
+        111.12  | "ciento once € 12/100"
+        112.12  | "ciento doce € 12/100"
+        123.29  | "ciento veintitrés € 29/100"
+        154.63  | "ciento cincuenta y cuatro € 63/100"
+        415.51  | "cuatrocientos quince € 51/100"
+        426.83  | "cuatrocientos veintiséis € 83/100"
+        447.60  | "cuatrocientos cuarenta y siete € 60/100"
+        1298    | "mil doscientos noventa y ocho € 00/100"
+        1299     | "mil doscientos noventa y nueve € 00/100"
+        1111111.11    | "un millón ciento once mil ciento once € 11/100"
     }
 }
