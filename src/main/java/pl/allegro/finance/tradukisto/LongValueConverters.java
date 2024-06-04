68d85767ac0bf23f698a344be55f6b85c5a87d5c
Stepanov Alexander <stepanov@fogsoft.ru>
2024-06-04 18:03:57 +0300

    russian long values converter

--- src/main/java/pl/allegro/finance/tradukisto/LongValueConverters.java
+++ src/main/java/pl/allegro/finance/tradukisto/LongValueConverters.java
@@ -3,17 +3,13 @@ package pl.allegro.finance.tradukisto;
 import pl.allegro.finance.tradukisto.internal.LongToStringConverter;
 import java.util.Objects;
 
-import static pl.allegro.finance.tradukisto.internal.Container.croatianContainer;
-import static pl.allegro.finance.tradukisto.internal.Container.englishContainer;
-import static pl.allegro.finance.tradukisto.internal.Container.polishContainer;
-import static pl.allegro.finance.tradukisto.internal.Container.hindiContainer;
-import static pl.allegro.finance.tradukisto.internal.Container.swedishContainer;
-import static pl.allegro.finance.tradukisto.internal.Container.japaneseKanjiContainer;
+import static pl.allegro.finance.tradukisto.internal.Container.*;
 
 public enum LongValueConverters {
 
     CROATIAN_LONG(croatianContainer().getLongConverter()),
     ENGLISH_LONG(englishContainer().getLongConverter()),
+    RUSSIAN_LONG(russianContainer().getLongConverter()),
     POLISH_LONG(polishContainer().getLongConverter()),
     HINDI_LONG(hindiContainer().getLongConverter()),
     SWEDISH_LONG(swedishContainer().getLongConverter()),
