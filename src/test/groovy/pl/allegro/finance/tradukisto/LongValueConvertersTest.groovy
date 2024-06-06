aa4131ae97e01940ead523c6092423f11c4aca91
Alex T <someone@example.com>
2024-06-06 19:59:17 +0300

    russian long value converter

--- src/test/groovy/pl/allegro/finance/tradukisto/LongValueConvertersTest.groovy
+++ src/test/groovy/pl/allegro/finance/tradukisto/LongValueConvertersTest.groovy
@@ -6,6 +6,7 @@ import spock.lang.Unroll
 import static pl.allegro.finance.tradukisto.LongValueConverters.ENGLISH_LONG
 import static pl.allegro.finance.tradukisto.LongValueConverters.HINDI_LONG
 import static pl.allegro.finance.tradukisto.LongValueConverters.POLISH_LONG
+import static pl.allegro.finance.tradukisto.LongValueConverters.RUSSIAN_LONG
 import static pl.allegro.finance.tradukisto.LongValueConverters.SWEDISH_LONG
 import static pl.allegro.finance.tradukisto.LongValueConverters.JAPANESE_KANJI_LONG
 
@@ -19,6 +20,7 @@ class LongValueConvertersTest extends Specification {
         where:
         language  | converter    || number
         "English" | ENGLISH_LONG || "one quintillion"
+        "Russian" | RUSSIAN_LONG || "один квинтиллион"
         "Polish"  | POLISH_LONG  || "jeden trylion"
         "Hindi"   | HINDI_LONG   || "दस शंख"
         "Swedish" | SWEDISH_LONG || "en triljon"
