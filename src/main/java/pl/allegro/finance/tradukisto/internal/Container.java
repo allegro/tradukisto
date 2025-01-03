22f96dca20ceda78e27d4352160e43fa4232ef45
Zhyrgalbek Sharshenov <93789294+SharshenovJR@users.noreply.github.com>
2025-01-03 21:29:56 +0600

    Added kyrgyz language (#166)
    * added kyrgyz language
    
    * Update README.md
    
    ---------
    
    Co-authored-by: SharshenovJR <sharshenov.jr>
    Co-authored-by: Julia Glaszka <85160468+jglaszka@users.noreply.github.com>

--- src/main/java/pl/allegro/finance/tradukisto/internal/Container.java
+++ src/main/java/pl/allegro/finance/tradukisto/internal/Container.java
@@ -30,6 +30,7 @@ import pl.allegro.finance.tradukisto.internal.languages.japanese.JapaneseNumberT
 import pl.allegro.finance.tradukisto.internal.languages.japanese.JapaneseThousandToWordsConverter;
 import pl.allegro.finance.tradukisto.internal.languages.japanese.JapaneseValues;
 import pl.allegro.finance.tradukisto.internal.languages.kazakh.KazakhValues;
+import pl.allegro.finance.tradukisto.internal.languages.kyrgyz.KyrgyzValues;
 import pl.allegro.finance.tradukisto.internal.languages.latvian.LatvianValues;
 import pl.allegro.finance.tradukisto.internal.languages.polish.PolishValues;
 import pl.allegro.finance.tradukisto.internal.languages.portuguese.BrazilianPortugueseValues;
@@ -268,6 +269,11 @@ public final class Container {
     public static Container kazakhContainer() {
         KazakhValues kazakhValues = new KazakhValues();
         return new Container(kazakhValues);
+    }
+
+    public static Container kyrgyzContainer() {
+        KyrgyzValues kyrgyzValues = new KyrgyzValues();
+        return new Container(kyrgyzValues);
     }
 
     public static Container hindiContainer() {
